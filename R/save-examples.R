bd3698bc6d508938be0ad10f9bf1bad478c39c1b
brshallo <brshallo@gmail.com>
2022-01-17 22:06:27 -0800

    created previews and unnested .csv versions

--- R/save-examples.R
+++ R/save-examples.R
@@ -1,6 +1,8 @@
 library(tidyverse)
 library(funspotr)
 
+### Parse R/Rmd files in github repos ###
+
 Sys.sleep(45 * 60)
 drob_tidy_tuesdays <- funspotr::github_spot_funs("dgrtwo/data-screencasts",
                                                  branch = "master",
@@ -16,10 +18,6 @@ brshallo_blog <- github_spot_funs("brshallo/brshallo",
                                   branch = "master",
                                   show_each_use = TRUE)
 
-fs::dir_create("data")
-readr::write_rds(drob_tidy_tuesdays, here::here("data", "drob-tidy-tuesdays-20220114.rds"))
-readr::write_rds(jsilge_blog, here::here("data", "jsilge-blog-20220114.rds"))
-readr::write_rds(brshallo_blog, here::here("data", "brshallo-blog-20220114.rds"))
 
 # R4DS book, only keep files that are chapters in book
 ch_files <- yaml::read_yaml("https://raw.githubusercontent.com/hadley/r4ds/main/_bookdown.yml") %>%
@@ -36,4 +34,53 @@ ch_urls <- ch_urls %>% # Arrange by order of chapters
 r4ds_book <- github_spot_funs(custom_urls = ch_urls,
                               show_each_use = TRUE)
 
+### Create data folders ###
+
+fs::dir_create("data")
+fs::dir_create("data", "previews")
+fs::dir_create("data", "funs")
+
+### Save RDS files ###
+
+readr::write_rds(drob_tidy_tuesdays, here::here("data", "drob-tidy-tuesdays-20220114.rds"))
+readr::write_rds(jsilge_blog, here::here("data", "jsilge-blog-20220114.rds"))
+readr::write_rds(brshallo_blog, here::here("data", "brshallo-blog-20220114.rds"))
 readr::write_rds(r4ds_book, here::here("data", "r4ds-chapter-files-20220117.rds"))
+
+#### Save previews ###
+
+drob_tidy_tuesdays %>%
+  select(-spotted) %>%
+  readr::write_csv(here::here("data", "previews", "drob-tidy-tuesdays-preview-20220114.csv"))
+
+jsilge_blog %>%
+  select(-spotted) %>%
+  readr::write_csv(here::here("data", "previews", "jsilge-blog-preview-20220114.csv"))
+
+brshallo_blog %>%
+  select(-spotted) %>%
+  readr::write_csv(here::here("data", "previews", "brshallo-blog-preview-20220114.csv"))
+
+r4ds_book %>%
+  select(-spotted) %>%
+  readr::write_csv(here::here("data", "previews", "r4ds-chapter-files-preview-20220117.csv"))
+
+
+### Save functions used ###
+### For each repo, have several URL's that errored or didn't have any R code -- so not every file parsed ###
+
+drob_tidy_tuesdays %>%
+  funspotr::unnest_github_results() %>%
+  readr::write_csv(here::here("data", "funs", "drob-tidy-tuesdays-funs-20220114.csv"))
+
+jsilge_blog %>%
+  funspotr::unnest_github_results() %>%
+  readr::write_csv(here::here("data", "funs", "jsilge-blog-funs-20220114.csv"))
+
+brshallo_blog %>%
+  funspotr::unnest_github_results() %>%
+  readr::write_csv(here::here("data", "funs", "brshallo-blog-funs-20220114.csv"))
+
+r4ds_book %>%
+  funspotr::unnest_github_results() %>%
+  readr::write_csv(here::here("data", "funs", "r4ds-chapter-files-funs-20220117.csv"))
--- README.md
+++ README.md
@@ -6,16 +6,24 @@
 This repository contains examples of using the package
 [funspotr](https://github.com/brshallo/funspotr) on a few github
 repositories. See “R/save-examples.R” for the code used to write files
-to “data” folder.
+to the “data” folders.
 
-Currently the data files are static .rds files. However in the future
-I’d like to use github actions to (potentially) make some of the data
-files located here update on a regular basis.
+Currently the data files are static files. However in the future I’d
+like to use github actions to (potentially) make some of the data files
+located online update on a regular basis.
 
-## Current .rds files
+## Current files
+
+.rds files of direct `funspotr::github_spot_funs()` can be found in
+“data”. .csv formmatted files can be found in “data/previews” and
+“data/funss” – the former contains files for each repo where parsing was
+attempted, the latter contains each instance a function was used and the
+file it came from.
 
 ``` r
-fs::dir_ls("data")
-#> data/brshallo-blog-20220114.rds      data/drob-tidy-tuesdays-20220114.rds 
-#> data/jsilge-blog-20220114.rds        data/r4ds-chapter-files-20220117.rds
+fs::dir_ls("data/funs")
+#> data/funs/brshallo-blog-funs-20220114.csv
+#> data/funs/drob-tidy-tuesdays-funs-20220114.csv
+#> data/funs/jsilge-blog-funs-20220114.csv
+#> data/funs/r4ds-chapter-files-funs-20220117.csv
 ```
