9c0323334f1a4cc2fd15f31ca1c4a64954529f8c
Bryan Shalloway <30487784+brshallo@users.noreply.github.com>
2023-10-11 14:27:43 -0700

    Update save-examples.R
    update example so doesn't conflict with current funspotr code.

--- R/save-examples.R
+++ R/save-examples.R
@@ -1,22 +1,38 @@
+## The approach used in this file has been deprecated and moved to unexported
+## functions. These functions are no longer exported by funspotr, hence change
+## to ::: syntax. Instead you would create `drob_tidy_tuesdays` object using a
+## more modular approach. The example below would produce essentially identical
+## output to that created using the deprecated functions:
+
+## UP-TO-DATE SYNTAX:
+# drob_tidy_tuesdays <-
+#   list_files_github_repo("dgrtwo/data-screencasts",
+#                          branch = "master") %>%
+#   spot_funs_files(show_each_use = TRUE,
+#                   keep_in_multiple_pkgs = TRUE) %>%
+#   unnest_results() %>% 
+#   rename(contents = relative_paths,
+#          urls = absolute_paths)
+
 library(dplyr)
 library(funspotr)
 
 ### Parse R/Rmd files in github repos ###
 
 Sys.sleep(45 * 60)
-drob_tidy_tuesdays <- funspotr::github_spot_funs("dgrtwo/data-screencasts",
-                                                 branch = "master",
-                                                 show_each_use = TRUE)
+drob_tidy_tuesdays <- funspotr:::github_spot_funs("dgrtwo/data-screencasts",
+                                                  branch = "master",
+                                                  show_each_use = TRUE)
 
 Sys.sleep(61 * 60)
-jsilge_blog <- funspotr::github_spot_funs("juliasilge/juliasilge.com",
-                                          branch = "master",
-                                          show_each_use = TRUE)
+jsilge_blog <- funspotr:::github_spot_funs("juliasilge/juliasilge.com",
+                                           branch = "master",
+                                           show_each_use = TRUE)
 
 Sys.sleep(61 * 60)
-brshallo_blog <- funspotr::github_spot_funs("brshallo/brshallo",
-                                            branch = "master",
-                                            show_each_use = TRUE)
+brshallo_blog <- funspotr:::github_spot_funs("brshallo/brshallo",
+                                             branch = "master",
+                                             show_each_use = TRUE)
 
 
 # R4DS book, only keep files that are chapters in book
@@ -24,9 +40,9 @@ ch_files <- yaml::read_yaml("https://raw.githubusercontent.com/hadley/r4ds/main/
   purrr::pluck("rmd_files")
 
 ch_urls <-
-  funspotr::github_spot_funs(repo = "hadley/r4ds",
-                             branch = "main",
-                             preview = TRUE) %>%
+  funspotr:::github_spot_funs(repo = "hadley/r4ds",
+                              branch = "main",
+                              preview = TRUE) %>%
   filter(contents %in% ch_files)
 
 ch_urls <- ch_urls %>%
@@ -35,8 +51,8 @@ ch_urls <- ch_urls %>%
   arrange(contents_fct) %>%
   select(-contents_fct)
 
-r4ds_book <- funspotr::github_spot_funs(custom_urls = ch_urls,
-                                        show_each_use = TRUE)
+r4ds_book <- funspotr:::github_spot_funs(custom_urls = ch_urls,
+                                         show_each_use = TRUE)
 
 ### Create data folders ###
 
@@ -75,19 +91,21 @@ r4ds_book %>%
 ### For each repo, have several URL's that errored or didn't have any R code -- so not every file parsed ###
 
 drob_tidy_tuesdays %>%
-  funspotr::unnest_github_results() %>%
+  # unnest_results() was previously unnest_github_results() ... example was updated to be consistent
+  # with current funspotr syntax
+  funspotr::unnest_results() %>%
   readr::write_csv(here::here("data", "funs", "drob-tidy-tuesdays-funs-20220114.csv"))
 
 jsilge_blog %>%
-  funspotr::unnest_github_results() %>%
+  funspotr::unnest_results() %>%
   readr::write_csv(here::here("data", "funs", "jsilge-blog-funs-20220114.csv"))
 
 brshallo_blog %>%
-  funspotr::unnest_github_results() %>%
+  funspotr::unnest_results() %>%
   readr::write_csv(here::here("data", "funs", "brshallo-blog-funs-20220114.csv"))
 
 r4ds_book %>%
-  funspotr::unnest_github_results() %>%
+  funspotr::unnest_results() %>%
   readr::write_csv(here::here("data", "funs", "r4ds-chapter-files-funs-20220117.csv"))
 
 ### Get package dependencies for each ###
@@ -95,39 +113,40 @@ r4ds_book %>%
 
 # Sys.sleep(45 * 60)
 
+# See above for up-to-date funspotr syntax... which is modularized compared to the examples here
 drob_tidy_tuesdays_deps <-
-  funspotr::github_spot_pkgs("dgrtwo/data-screencasts",
-                             branch = "master")
+  funspotr:::github_spot_pkgs("dgrtwo/data-screencasts",
+                              branch = "master")
 
 # Sys.sleep(61 * 60)
 jsilge_blog_deps <-
-  funspotr::github_spot_pkgs("juliasilge/juliasilge.com",
-                             branch = "master")
+  funspotr:::github_spot_pkgs("juliasilge/juliasilge.com",
+                              branch = "master")
 
 # Sys.sleep(61 * 60)
-brshallo_blog_deps <- funspotr::github_spot_pkgs("brshallo/brshallo",
-                                                 branch = "master")
+brshallo_blog_deps <- funspotr:::github_spot_pkgs("brshallo/brshallo",
+                                                  branch = "master")
 
-r4ds_book_deps <- funspotr::github_spot_pkgs(custom_urls = ch_urls)
+r4ds_book_deps <- funspotr:::github_spot_pkgs(custom_urls = ch_urls)
 
 ### Write package dependencies to .csv ###
 
 drob_tidy_tuesdays_deps %>%
-  unnest_github_results() %>%
+  unnest_results() %>%
   rename(pkgs = spotted) %>%
   readr::write_csv(here::here("data", "deps", "drob-tidy-tuesdays-deps-20220118.csv"))
 
 jsilge_blog_deps %>%
-  unnest_github_results() %>%
+  unnest_results() %>%
   rename(pkgs = spotted) %>%
   readr::write_csv(here::here("data", "deps", "jsilge-blog-deps-20220118.csv"))
 
 brshallo_blog_deps %>%
-  unnest_github_results() %>%
+  unnest_results() %>%
   rename(pkgs = spotted) %>%
   readr::write_csv(here::here("data", "deps", "brshallo-blog-deps-20220118.csv"))
 
 r4ds_book_deps %>%
-  unnest_github_results() %>%
+  unnest_results() %>%
   rename(pkgs = spotted) %>%
   readr::write_csv(here::here("data", "deps", "r4ds-chapter-files-deps-20220118.csv"))
